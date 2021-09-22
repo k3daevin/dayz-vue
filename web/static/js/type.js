d101955ca5e32f0c0417d3db248d3b2d38f9d8f6
k3daevin <kevindrzyc@gmail.com>
2021-09-22 14:23:59 +0200

    alle types rendern und anfang einzelne editen

--- web/static/js/type.js
+++ web/static/js/type.js
@@ -1,9 +1,11 @@
 let type = class Type {
     constructor(xml) {
+        this.visible = true;
+        this.editable = false;
         function singleText(name) {
             return parseInt(xml.getElementsByTagName(name)[0].textContent)
         }
-        function getName(elem) {
+        function getNameAttribute(elem) {
             return elem.attributes.name.value
         }
         this.name = xml.attributes["name"].value
@@ -23,13 +25,38 @@ let type = class Type {
         this.usage = []
         let usageElems = xml.getElementsByTagName('usage')
         for (let i = 0; i < usageElems.length; ++i) {
-            this.usage.push(usageElems[i])
+            this.usage.push(getNameAttribute(usageElems[i]))
         }
         this.value = []
         let valueElems = xml.getElementsByTagName('value')
         for (let i = 0; i < valueElems.length; ++i) {
-            this.value.push(valueElems[i])
+            this.value.push(getNameAttribute(valueElems[i]))
         }
 
     }
-} 
+}
+
+Vue.component('type-flag', {
+    props: ['icon', 'value'],
+    template: `
+    <td>
+        <div v-if="value">
+            <b :class="icon" />            
+        </div>
+        <div v-else>
+            <i :class="icon" style="color:lightgrey;"/>
+        </div>
+    </td>
+    `
+  })
+Vue.component('mannebox', {
+    props: ['type', 'property', 'editable'],
+    template : `
+    <div v-if="editable">
+      <input v-model="type[property]"></input>
+    </div>
+    <div v-else>
+      {{ type[property] }}
+    </div>
+    `
+})
